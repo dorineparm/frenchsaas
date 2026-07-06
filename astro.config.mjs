1fc53ea313298240c36501d8b1c051adcb6e5d8e
dorineparm <98556843+dorineparm@users.noreply.github.com>
2026-07-06 17:30:13 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
@@ -6,12 +5,3 @@ export default defineConfig({
   site: 'https://frenchsaas.com',
   integrations: [sitemap()],
 });
-=======
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-
-export default defineConfig({
-  site: "https://frenchsaas.com",
-  integrations: [sitemap()],
-});
->>>>>>> 405f3ce (Configure Astro sitemap)
